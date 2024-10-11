26d3a9fbdec6a56191f6ab2811a9f0a64d54670f
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2024-10-11 06:48:36 +0300

    use default configrations for next config file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	// reactStrictMode: true,
-	// webpack: (config) => {
-	// 	config.resolve.extensions.push(".ts", ".tsx");
-	// 	return config;
-	// },
+	reactStrictMode: true,
+	webpack: (config) => {
+		config.resolve.extensions.push(".ts", ".tsx");
+		return config;
+	},
 };
 
 module.exports = nextConfig;
